d49c628c136d16f040ebac509e53a120db669ba3
Sunny Golovine <sunny@glvn.co>
2021-02-06 12:33:02 -0500

    Configure dotenv

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,4 +1,19 @@
-exports.handler = function (event, context, callback) {
+require("dotenv").config();
+
+exports.handler = function (event, _context, callback) {
+  console.log("TEST_VAR value", process.env.TEST_VAR);
+  try {
+    const bodyContent = JSON.parse(event.body);
+    const { name, email, message } = bodyContent;
+    if (!name || !email || !message) {
+      callback(null, { statusCode: 400, body: "Missing params" });
+      return;
+    }
+    callback(/** error */ null, { statusCode: 200, body: "Success!" });
+  } catch {
+    callback(null, { statusCode: 400, body: "Error!" });
+    return;
+  }
+
   // More stuff to be done here
-  callback(/** error */ null, { statusCode: 200, body: "Success!" });
 };
