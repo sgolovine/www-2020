7496a3ab4fc563948295d631c2b365dbbc82794b
Sunny Golovine <sunny@glvn.co>
2021-02-08 10:50:24 -0500

    WIP

--- src/components/home/ProfilePicture.tsx
+++ src/components/home/ProfilePicture.tsx
@@ -8,12 +8,7 @@ type Props = {
 const ProfilePicture: React.FC<Props> = ({ headline }) => {
   return (
     <div className="max-w-xs mx-auto text-center">
-      <img
-        src={profilePictureSrc}
-        alt="Profile Picture"
-        width={256}
-        height={256}
-      />
+      <img src={profilePictureSrc} alt="Profile Picture" />
       <h1 className="text-3xl mt-6">{headline}</h1>
     </div>
   );
