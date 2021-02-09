5ce9b6a54425d200f9a17a92990f0a8e012d1ab2
Sunny Golovine <sunny@glvn.co>
2021-02-08 20:58:18 -0500

    Add landing page:

---
+++ src/pages/landing/links.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageHeader from "~/components/common/PageHeader";
+
+const Links = () => {
+  return (
+    <div>
+      <PageHeader headerText="Blank Landing Page" />
+      <p>This is a blank, hidden landing page</p>
+    </div>
+  );
+};
+
+export default Links;
