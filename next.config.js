f323136b49b1da67e218cc31d47a91f269a4d14a
Sunny Golovine <sunny@glvn.co>
2021-02-12 11:28:27 -0500

    Remove redirect in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,4 @@ const withOptimizedImages = require("next-optimized-images");
 module.exports = withOptimizedImages({
   optimizeImages: true,
   optimizeImagesInDev: true,
-  async redirects() {
-    return [
-      {
-        source: "/dev",
-        destination: process.env.NODE_ENV === "production" ? "/" : "/dev",
-        permanent: true,
-      },
-    ];
-  },
 });
