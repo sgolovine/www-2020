3a7e1310d1ccc0fd0eb98cce56f22e8d789e0f0d
Sunny Golovine <sunny@glvn.co>
2021-02-16 16:52:40 -0500

    Update layout, fix layout for contact page

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Children, ReactNode } from "react";
 import useContactForm from "~/hooks/useContactForm";
 import CheckCircle from "../icons/CheckCircle";
 import ErrorCircle from "../icons/ErrorCircle";
@@ -12,69 +12,86 @@ const ContactForm = () => {
     setFormField,
   } = useContactForm();
 
-  return (
-    <div>
-      <div className="flex flex-col p-2">
-        <div className="h-6">
-          {messageState.show && (
-            <div className="flex flex-row">
-              <div className="w-5">
-                {messageState.type === "success" ? (
-                  <CheckCircle />
-                ) : (
-                  <ErrorCircle />
-                )}
-              </div>
-              <p
-                className={`ml-1 text-sm font-bold ${
-                  messageState.type === "success"
-                    ? "text-green-600"
-                    : "text-red-600"
-                }`}
-              >
-                {messageState.message}
-              </p>
+  const renderStatus = () => {
+    return (
+      <div className="h-6">
+        {messageState.show && (
+          <div className="flex flex-row">
+            <div className="w-5">
+              {messageState.type === "success" ? (
+                <CheckCircle />
+              ) : (
+                <ErrorCircle />
+              )}
             </div>
-          )}
-        </div>
-        <label className="text-sm font-bold">Your Name</label>
+            <p
+              className={`ml-1 text-sm font-bold ${
+                messageState.type === "success"
+                  ? "text-green-600"
+                  : "text-red-600"
+              }`}
+            >
+              {messageState.message}
+            </p>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  const Section = ({
+    label,
+    children,
+  }: {
+    label: string;
+    children: ReactNode;
+  }) => {
+    return (
+      <div className="flex flex-col p-2">
+        <label className="text-sm font-bold">{label}</label>
+        {children}
+      </div>
+    );
+  };
+
+  return (
+    <div className="w-full sm:w-initial">
+      {renderStatus()}
+      <Section label="Your Name">
         <input
           value={form.name}
           onChange={(e) => setFormField("name", e.target.value)}
           placeholder="John Doe"
           className="border rounded p-2 max-w-lg"
         />
-      </div>
-      <div className="flex flex-col p-2">
-        <label className="text-sm font-bold">Your Email</label>
+      </Section>
+      <Section label="Your Email">
         <input
           placeholder="johndoe@gmail.com"
           className="border rounded p-2 max-w-lg"
           value={form.email}
           onChange={(e) => setFormField("email", e.target.value)}
         />
-      </div>
-      <div className="flex flex-col p-2">
-        <label className="text-sm font-bold">Message</label>
+      </Section>
+      <Section label="Message">
         <textarea
           value={form.message}
           onChange={(e) => setFormField("message", e.target.value)}
           placeholder="Hey Sunny..."
           className="border rounded p-2 max-w-lg"
           rows={7}
-          cols={50}
         />
-      </div>
-      <div className="p-2 flex flex-row justify-around">
+      </Section>
+      <div className="p-2 flex flex-col sm:flex-row">
         <button
           onClick={handleSubmit}
-          className="p-2 bg-blue-500 w-36 rounded text-white font-bold mr-1"
+          className="p-2 bg-blue-500 sm:w-36 rounded text-white font-bold sm:mr-1 my-2 sm:my-initial"
         >
           Send
         </button>
         <button
           onClick={handleClear}
-          className="p-2 bg-red-500 w-36 rounded text-white font-bold ml-1"
+          className="p-2 bg-red-500 sm:w-36 rounded text-white font-bold sm:ml-1 my-2 sm:my-initial"
         >
           Clear
         </button>
