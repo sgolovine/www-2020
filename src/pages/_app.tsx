eab6241718d255a5b409c4b8512cdb78af129026
Sunny Golovine <sunny@glvn.co>
2021-02-17 11:00:27 -0500

    Remove links code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,16 @@ import { AppProps } from "next/app";
 import Layout from "~/components/site/Layout";
 import { SiteContextProvider } from "~/context/SiteContext";
 import { ResumeContextProvider } from "~/context/ResumeContext";
-import { LinksContextProvider } from "~/context/LinksContext";
 
 import "~/styles/tailwind.css";
-import "~/styles/links.css";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <SiteContextProvider>
       <ResumeContextProvider>
-        <LinksContextProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </LinksContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ResumeContextProvider>
     </SiteContextProvider>
   );
