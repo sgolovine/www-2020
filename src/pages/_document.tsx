16597827a9e99f66a2654d9e16dfe98beeb6719c
Sunny Golovine <sunny@glvn.co>
2021-04-18 19:03:20 -0400

    Add title desc and robots meta to head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,18 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class AppDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <title>Sunny Golovine</title>
+
           <link rel="shortcut icon" href="/img/favicon.ico" />
+
+          <meta
+            name="description"
+            content="Personal Website of Sunny Golovine"
+          />
+
+          <meta name="robots" content="follow,index" />
         </Head>
         <body>
           <Main />
