1de9eb7e9519e8b489246f2dd9395e40ba326c0f
Sunny Golovine <sunny@glvn.co>
2021-04-18 14:13:33 -0400

    Implement Preact

--- next.config.js
+++ next.config.js
@@ -7,4 +7,15 @@ module.exports = withOptimizedImages({
     webpack5: true,
     strictPostcssConfiguration: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
 });
